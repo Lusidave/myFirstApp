df2e50f0e3131056edfdc0a64b1bf5cc7c8ef1a2
Lusidave <lusidave@hotmail.fr>
2020-08-19 22:19:41 +0200

    title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myNewApp';
+  title = 'Welcome to Lusidave website !';
 }
